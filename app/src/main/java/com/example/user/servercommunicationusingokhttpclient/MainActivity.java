3ba3b41f7709a5a07a6b0a7e190a2ddd914b8540
swathi-s <swathi.ehr@gmail.com>
2016-09-15 11:06:25 +0530

    AsyncTask class is added to handle the network related functionality

--- app/src/main/java/com/example/user/servercommunicationusingokhttpclient/MainActivity.java
+++ app/src/main/java/com/example/user/servercommunicationusingokhttpclient/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.servercommunicationusingokhttpclient;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,28 +23,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendRequest(View view) {
-        OkHttpClient client = new OkHttpClient();
+        new GetDataFromServer().execute();
+    }
 
-        //For normal get method
+    class GetDataFromServer extends AsyncTask{
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+            OkHttpClient client = new OkHttpClient();
+
+            //For normal get method
         /*Request request = new Request.Builder()
                 .url("http://192.168.1.7/androidService.php")
                 .build();*/
+            Log.d("response","okHttpClient instance is created");
+            //for sending the query string to the get method
+            HttpUrl.Builder urlBuilder = HttpUrl.parse("https://api.github.help").newBuilder();
+            urlBuilder.addQueryParameter("v","1.0");
+            urlBuilder.addQueryParameter("user","vogella");
+            String url = urlBuilder.build().toString();
 
-        //for sending the query string to the get method
-        HttpUrl.Builder urlBuilder = HttpUrl.parse("http://192.168.1.7/androidService.php").newBuilder();
-        urlBuilder.addQueryParameter("data","twenty");
-        String url = urlBuilder.build().toString();
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+            try {
+                Response response = client.newCall(request).execute();
+                Toast.makeText(getApplicationContext(),"response got",Toast.LENGTH_SHORT).show();
+                Log.d("response",response.body().toString());
 
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-
-        try {
-            Response response = client.newCall(request).execute();
-            Log.d("response",response.body().toString());
-        } catch (IOException e) {
-            Toast.makeText(this, "exception is "+e, Toast.LENGTH_SHORT).show();
-            e.printStackTrace();
+            } catch (IOException e) {
+                Toast.makeText(getApplicationContext(),"exception is "+e,Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+            return null;
         }
     }
 }
